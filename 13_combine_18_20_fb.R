f35c681ae1537e932cc4e59787e34a23518dd048
Markus Neumann <mneumann.polsci@gmail.com>
2022-08-08 23:02:45 -0400

    fix column order in fb training data

--- 13_combine_18_20_fb.R
+++ 13_combine_18_20_fb.R
@@ -15,8 +15,16 @@ fb20 <- fb20 %>% rename(alt = ad_id)
 
 # Combine
 fb <- bind_rows(fb18, fb20)
-fb <- fb[fb$transcript != '',]
-fb <- fb[fb$transcript != 'NA',]
-fb <- fb[is.na(fb$transcript) == F,]
+# fb <- fb[fb$transcript != '',]
+# fb <- fb[fb$transcript != 'NA',]
+# fb <- fb[is.na(fb$transcript) == F,]
+
+# Only relevant columns, and in the right order
+p_issues_of_interest <- "data/issues_of_interest.csv"
+issues <- fread(p_issues_of_interest)
+issues <- issues$issue_code[issues$issue_frequency>=100]
+issues <- issues[order(as.integer(str_remove(issues, "ISSUE")))] # sort issue columns
+issues <- issues[!issues %in% c("ISSUE116", "ISSUE209")]
+fb <- fb[,c('alt', 'transcript', issues)]
 
 fwrite(fb, "data/fb_18_20_for_imputation.csv")
