98a4211a82d89c4c0ede872d70e1c4efcce48462
Markus Neumann <mneumann.polsci@gmail.com>
2022-08-08 22:58:38 -0400

    fix fb 2020 and add some data

--- 12_fb20.R
+++ 12_fb20.R
@@ -37,11 +37,13 @@ fb20 <- fb20 %>%
   select(ad_id, ad_creative_body, asr, c(starts_with("ISSUE"))) %>% # keep relevant cols
   mutate(across(c(ad_creative_body, asr), fix_unicode)) %>%
   mutate(across(c(ad_creative_body, asr), clean_brackets)) %>%
-  unite("transcript", c(ad_creative_body, asr), sep = " ") %>% # combine acb and asr
+  unite("transcript", c(ad_creative_body, asr), sep = " ", na.rm = T) %>% # combine acb and asr
   mutate(across(-c(ad_id, transcript), function(x){ifelse(x == "False", "", x)})) %>% # make quasi-boolean columns conform to the rest
   mutate(across(-c(ad_id, transcript), function(x){ifelse(x == "", 0, 1)})) %>% # binarize
   mutate(ad_id = str_replace(ad_id, "x_", "fb20-")) %>%
-  rename(alt = ad_id)
+  rename(alt = ad_id) %>%
+  mutate(transcript = str_squish(transcript)) %>% 
+  filter(transcript != "")
 
 #fwrite(fb20, "data/fbel_prepared_issues.csv")
 save(fb20, file = "data/fb_2020.rdata")
