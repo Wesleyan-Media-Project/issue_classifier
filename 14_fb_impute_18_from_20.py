510328ccfd177d403bf2fad5bf90b48eedaf43d3
Markus Neumann <mneumann.polsci@gmail.com>
2022-08-08 23:15:49 -0400

    updated FB imputation

--- 14_fb_impute_18_from_20.py
+++ 14_fb_impute_18_from_20.py
@@ -12,42 +12,36 @@ import numpy as np
 from joblib import dump, load
 
 df = pd.read_csv("data/fb_18_20_for_imputation.csv")
-issues = pd.read_csv("data/issues_of_interest.csv")
-issues = issues[issues['issue_frequency']>=100]
-new_issues = issues['issue_code'][issues['label18'].isna()].values
-new_issues = [n for n in new_issues if n not in ['ISSUE209', 'ISSUE116']] # Kick out issues that have problematic codings and are subsets of issues 53/55 anyway
 
-traintest = df.dropna(axis = 0)
-inference = df[df['ISSUE209'].isna()]
+main_cols = ["alt", "transcript"]
+issue_cols = [c for c in df.columns if 'ISSUE' in c]
 
-train, test = ms.train_test_split(traintest, random_state = 123)
+# Loop over all issues
+for g in issue_cols:
 
-
-clf_rf = Pipeline([('vect', CountVectorizer()),
-                    ('tfidf', TfidfTransformer()),
-                    ('cal', CalibratedClassifierCV(RandomForestClassifier(n_estimators=500, random_state = 123), cv=2, method="sigmoid"),)
-])
-
-# Train binary issue classifiers for 2020 and then apply to 2018
-# Not saving the models since training is fairly quick and models are large
-for g in new_issues:
+  # Subset to only the relevant column
+  traintestinf = df[main_cols + [g]]
+  # Separate all the rows without NAs
+  # and make them the train/test set
+  traintest = traintestinf.dropna(axis = 0)
+  # The rows with NAs are the inference set
+  inference = traintestinf[traintestinf[g].isna()]
   
-  print(g)
-  # Train
-  clf_rf.fit(train['transcript'], train[g])
-  # Test
-  predicted = clf_rf.predict(test['transcript'])
-  print(metrics.classification_report(test[g], predicted))
-  # Impute
-  inference[g] = clf_rf.predict(inference['transcript'])
+  # If the inference set contains more than 0 rows
+  if len(inference) > 0:
+    
+      # Split
+      train, test = ms.train_test_split(traintest, train_size = 0.9)
+      # Train & test
+      clf_rf = Pipeline([('vect', CountVectorizer()),
+                          ('tfidf', TfidfTransformer()),
+                          ('cal', CalibratedClassifierCV(RandomForestClassifier(n_estimators=500, random_state = 123), cv=2, method="sigmoid"),)
+      ])
+      clf_rf.fit(train['transcript'], train[g])
+      predicted = clf_rf.predict(test['transcript'])
+      print(metrics.classification_report(test[g], predicted))
+      # Impute
+      df.loc[inference.index, g] = clf_rf.predict(inference['transcript'])
+  
 
-
-# Re-combine 18 and 20 sets
-df_imputed = pd.concat([traintest, inference])
-df_imputed = df_imputed.drop(columns = ['ISSUE209', 'ISSUE116'])
-# Only keep issues we actually care about
-relevant_issues = [n for n in issues['issue_code'] if n not in ['ISSUE209', 'ISSUE116']]
-relevant_cols = ['alt'] + ['transcript'] + relevant_issues
-df_imputed = df_imputed[relevant_cols]
-
-df_imputed.to_csv("data/fb_18_20_imputed.csv", index = False)
+df.to_csv("data/fb_18_20_imputed.csv", index = False)
