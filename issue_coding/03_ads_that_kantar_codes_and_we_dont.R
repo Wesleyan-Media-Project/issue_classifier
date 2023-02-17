937f51ca2c076aada93be3b1ffc3cf429376a86c
IneshV <123998765+IneshV@users.noreply.github.com>
2023-02-17 17:42:40 -0500

    Update 03_ads_that_kantar_codes_and_we_dont.R
    This one works I think but the last one did not...

--- issue_coding/03_ads_that_kantar_codes_and_we_dont.R
+++ issue_coding/03_ads_that_kantar_codes_and_we_dont.R
@@ -8,11 +8,13 @@ library(haven)
 map <- fread("data/compare_2020_cmag_wmp_issue_frequency.csv")
 df <- read_dta("../../datasets/tv/2020_creative_level_122120.dta")
 alphas <- readxl::read_xlsx("data/2020_TV_Alphas_Final.xlsx")
+
 map2 <- fread("data/mapping_cmag_wmp_issues.csv", header = T)
 
 #Cleans the data in map by removing the rows that contain the characters "=|&" or are empty in the "Related WMP var" column.
 map2 <- map2[!str_detect(map2$`Related WMP var`, '=|\\&'),]
 map2 <- map2[map2$`Related WMP var` != "",]
+
 
 # ASR
 p_asr <- "../data/tv_2020_asr.csv"
@@ -32,6 +34,7 @@ df <- left_join(df, df_asr, by = c('alt' = 'filename'))
 df <- df[is.na(df$transcript)==F,]
 
 missing_social_abortion <- list()
+
 missing_ISSUE30 <- list()
 
 
@@ -43,7 +46,6 @@ for (i in 1:nrow(df)) {
   }
 }
 
-#Also only inputs 0s twice
 for (i in 1:nrow(df)) {
   if (is.na(df$ISSUE30[i])) {
     missing_ISSUE30 <- c(missing, df$alt[i])
@@ -51,8 +53,27 @@ for (i in 1:nrow(df)) {
   }
 }
 
+
+
+#Old way of replacing missing values with 0 in the "issue_social_abortion" and "ISSUE30" columns
+#df$issue_social_abortion[is.na(df$issue_social_abortion)] <- 0
+#df$ISSUE30[is.na(df$ISSUE30)] <- 0
+
+
 #Creating a new variable or column "only_K_abort" using a logical expression based on the values in "issue_social_abortion" and "ISSUE30" columns
 df$only_K_abort <- (df$issue_social_abortion == 1) & (df$ISSUE30 == 0)
+
+df0 <- df[,match(map$wmp_var, names(df))]
+
+
+#df1 <- df[,c(match(map2$wmp_var, names(df)), which(names(df) %in% c("alt", "transcript", "link")))]
+#Check if df vs df_not_kantar is supposed to be on the left or right or both  
+#df2 <- df[,c(match(map2$`Related WMP var`, names(df)), which(names(df) %in% c("alt", "transcript", "link")))]
+
+#df_merged <- merge(df1, df2, by = "link", all = TRUE)
+
+missing_wmp <- list()
+missing_cmag <- list()
 
 
 if (!dir.exists("data/ads_where_kantar_wmp_disagree")) {
@@ -68,6 +89,25 @@ for(i in 1:nrow(map)){
     
     issue_wmp <- map$issue_wmp[i]
     issue_cmag <- map$issue_cmag[i]
+    
+    
+    # I am very confused as to why this doesnt work but I think the number of times where 0 is inputted should be accounted for 
+    #for (i in 1:nrow(df)) {
+    #  if (is.na(df[i, issue_cmag])) {
+    #    missing_cmag[[length(missing_cmag) + 1]] <- df[i, "alt"]
+    #    df <- df[-i, ]
+    #  }
+    #}
+    
+    #for (i in 1:nrow(df)) {
+    #  if (is.na(df[i, issue_wmp])) {
+    #    missing_wmp[[length(missing_wmp) + 1]] <- df[i, "alt"]
+    #    df <- df[-i, ]
+    #  }
+    #}
+    
+    
+    
     
     df[issue_cmag][is.na(df[issue_cmag])] <- 0
     df[issue_wmp][is.na(df[issue_wmp])] <- 0
@@ -91,13 +131,20 @@ for(i in 1:nrow(map)){
     
     
     #Filtering df for only the rows where discrep is true and selecting columns
-    df_discrep <- temp_df %>%
-      select(alt, transcript, link, issue_wmp, issue_cmag
+    discrep <- ((df[issue_cmag] == 1) & (df[issue_wmp] == 0)) | ((df[issue_cmag] == 0) & (df[issue_wmp] == 1))
+    
+    df_discrep <- df %>%
+      select(alt, transcript, link, issue_wmp, issue_cmag)
+    
     
     fwrite(df_discrep, paste0("data/ads_where_kantar_wmp_disagree/", issue_cmag, ".csv"))
+    
+    
     
   }
 }
 
 
 #issues <- fread("../data/issues_of_interest.csv")
+
+
