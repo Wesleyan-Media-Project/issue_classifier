ebd7565208e3596f82880664174db6a8cfd402d4
IneshV <123998765+IneshV@users.noreply.github.com>
2023-02-24 11:50:38 -0500

    Update 08_text_coding_precision_and_recall.R

--- issue_coding/08_text_coding_precision_and_recall.R
+++ issue_coding/08_text_coding_precision_and_recall.R
@@ -15,25 +15,26 @@ for (file_name in file_names) {
   df <- read.csv(file.path(folder_path, file_name))
   
   # Get the columns with names that start with "ISSUE" or "issue"
-  wmp_col <- colnames(df)[grepl("^ISSUE", colnames(df))]
+  #wmp_col <- colnames(df)[grepl("^ISSUE", colnames(df))]
+  kantar_col <- colnames(df)[grepl("^issue", colnames(df))]
   combined_col <- colnames(df)[grepl("^combined", colnames(df))]
   
 
   # Calculate precision and recall
-  tp <- sum(df[,combined_col] == df[,wmp_col] & df[,wmp_col] == 1)
-  fp <- sum(df[,combined_col] != df[,wmp_col] & df[,wmp_col] == 1)
-  fn <- sum(df[,combined_col] != df[,wmp_col] & df[,wmp_col] == 0)
+  tp <- sum(df[,combined_col] == df[,kantar_col] & df[,kantar_col] == 1)
+  fp <- sum(df[,combined_col] != df[,kantar_col] & df[,kantar_col] == 1)
+  fn <- sum(df[,combined_col] != df[,kantar_col] & df[,kantar_col] == 0)
   precision <- tp / (tp + fp)
   recall <- tp / (tp + fn)
   
   #Count the number of 1s in the kantar and wmp columns
   
-  wmp_total_ones <- sum(df[,wmp_col] == 1)
+  kantar_total_ones <- sum(df[,kantar_col] == 1)
   text_total_ones <- sum(df[,combined_col] == 1)
   
   
   # Add a row to the results dataframe with the calculated values
-  results_df <- rbind(results_df, data.frame(kantar_issue = names(df)[5], wmp_issue = names(df)[4], precision, recall, wmp_total_ones, text_total_ones))
+  results_df <- rbind(results_df, data.frame(kantar_issue = names(df)[5], wmp_issue = names(df)[4], precision, recall, kantar_total_ones, text_total_ones))
 }
 
 # Write the results dataframe to a CSV file
