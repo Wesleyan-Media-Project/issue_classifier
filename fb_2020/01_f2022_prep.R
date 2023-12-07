b9f5834c9debbfc35d25a2e727af78af2ba36778
furkmak <93638016+furkmak@users.noreply.github.com>
2023-12-07 00:38:55 -0500

    Create 01_f2022_prep.R

---
+++ fb_2020/01_f2022_prep.R
@@ -0,0 +1,23 @@
+library(data.table)
+library(tidyverse)
+
+path_f22 <- "../fb_2022/fb_2022_adid_text_clean.csv.gz"
+
+out_f22 <- "fb_2022/data/fb_22_for_inf.csv"
+
+f22 <- fread(path_f22, encoding = "UTF-8")
+
+# Aggregate
+f22_2 <- f22 %>% 
+  pivot_longer(-ad_id) %>%
+  filter(value != "") %>%
+  mutate(id = paste(ad_id, name, sep = "__")) %>%
+  select(-c(ad_id, name))
+
+f22_2 <- aggregate(f22_2$id, by = list(f22_2$value), c)
+
+names(f22_2) <- c("text", "id")
+
+
+# Save
+fwrite(f22_2, out_f22)
