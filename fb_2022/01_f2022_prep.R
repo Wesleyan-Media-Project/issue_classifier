efeae1e9ddd63c320f2ac8f53bda108deb8f0a69
furkmak <93638016+furkmak@users.noreply.github.com>
2025-01-03 15:37:42 -0800

    Add files via upload

--- fb_2022/01_f2022_prep.R
+++ fb_2022/01_f2022_prep.R
@@ -1,23 +1,19 @@
 library(data.table)
-library(tidyr)
-library(dplyr)
+library(tidyverse)
 
-path_f22 <- "fb_2022_adid_text.csv.gz"
-# source : data-post-production/01-merge-results/01_merge_preprocessed_results
+path_f22 <- "../fb_2022/fb_2022_adid_text.csv.gz"
 
 out_f22 <- "fb_2022/data/fb_22_for_inf.csv"
 
 f22 <- fread(path_f22, encoding = "UTF-8")
 
-f22 <- f22 %>% select(
-  ad_id, page_name, disclaimer, ad_creative_body,
-  ad_creative_link_caption, ad_creative_link_title,
-  ad_creative_link_description, google_asr_text,
-  aws_ocr_text_img, aws_ocr_text_vid
-)
+f22_1 <- f22 %>% 
+  select(c(ad_id, ad_creative_body, google_asr_text, aws_ocr_text_img, 
+           aws_ocr_text_vid, page_name, disclaimer, ad_creative_link_caption, 
+           ad_creative_link_title, ad_creative_link_description))
 
 # Aggregate
-f22_2 <- f22 %>%
+f22_2 <- f22_1 %>% 
   pivot_longer(-ad_id) %>%
   filter(value != "") %>%
   mutate(id = paste(ad_id, name, sep = "__")) %>%
@@ -29,6 +25,4 @@ names(f22_2) <- c("text", "id")
 
 
 # Save
-fb_2022_data <- "fb_2022/data"
-dir.create(fb_2022_data)
 fwrite(f22_2, out_f22)
