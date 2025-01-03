ea529de36770f8636d3994cccc9a827f951eff40
furkmak <93638016+furkmak@users.noreply.github.com>
2025-01-03 15:38:33 -0800

    Add files via upload

--- google_2022/01_g2022_prep.R
+++ google_2022/01_g2022_prep.R
@@ -1,39 +1,27 @@
 library(data.table)
-library(tidyr)
-library(dplyr)
+library(tidyverse)
 
-path_g22 <- "g2022_adid_01062021_11082022_text.csv.gz"
-# this is the output table from part of data-post-production repo that merges preprocessed results.
-# Source: data-post-production/01-merge-results/01_merge_preprocessed_results
+path_g22 <- "../google_2022/g2022_adid_text.csv.gz"
+
 out_g22 <- "google_2022/data/google_22_for_inf.csv"
 
 g22 <- fread(path_g22, encoding = "UTF-8")
 
-
 # Subset
 g22_2 <- g22 %>%
-  select(c(
-    ad_id, ad_title, google_asr_text, aws_ocr_video_text,
-    aws_ocr_img_text, ad_text
-  ))
+  select(c(ad_id, ad_title, google_asr_text, aws_ocr_video_text, description,
+           aws_ocr_img_text, advertiser_name, ad_text))
 
 # Aggregate
-g22_3 <- g22_2 %>%
+g22_3 <- g22_2 %>% 
   pivot_longer(-ad_id) %>%
   filter(value != "") %>%
   mutate(id = paste(ad_id, name, sep = "__")) %>%
   select(-c(ad_id, name))
 
-# Add the concatenation step
-g22_4 <- g22_3 %>%
-  group_by(value) %>%
-  summarize(id = paste(id, collapse = " | ")) %>%
-  ungroup()
+g22_4 <- aggregate(g22_3$id, by = list(g22_3$value), c)
 
 names(g22_4) <- c("text", "id")
 
 # Save
-g_2022_data <- "google_2022/data"
-dir.create(g_2022_data)
-
 fwrite(g22_4, out_g22)
